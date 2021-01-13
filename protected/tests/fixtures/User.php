3f951b9fb8e1227753b71956f35698ca5123ff5f
srosanatalia <srosanatalia@gmail.com>
2021-01-12 21:19:23 -0300

    Inicializando projeto

---
+++ protected/tests/fixtures/User.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+	/*
+	'sample1'=>array(
+		'username' => '',
+		'PASSWORD' => '',
+		'email' => '',
+	),
+	'sample2'=>array(
+		'username' => '',
+		'PASSWORD' => '',
+		'email' => '',
+	),
+	*/
+);
